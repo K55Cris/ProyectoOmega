ff61f1f4ab42fd89d965d601073f179a9191eb56
SilentDefault <silentdefault@gmail.com>
2017-12-15 17:53:38 -0600

    MainMenu v2
    *leer el enterior Commit
    -Ahora tiene el logotipo y no un texto feo.
    -agrege condicionales en el background para decifir con 2 variables a
    donde se desliza (arriba-abajo, izquierda-derecha).

--- Proyecto_Omega/Assets/scripts/BackgroundScrolling.cs
+++ Proyecto_Omega/Assets/scripts/BackgroundScrolling.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundScrolling : MonoBehaviour {
+public class BackgroundScrolling : MonoBehaviour
+{
 
-    public float Speed;
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 offset = new Vector3(Time.time * Speed, Time.time * Speed);
+    public float Speed = 0.4f;
+    public bool toUp = false;
+    public bool toLeft = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * Speed * (toLeft ? -1 : 1), Time.time * Speed * (toUp ? -1 : 1));
         GetComponent<Renderer>().material.mainTextureOffset = offset;
-	}
+    }
 }
