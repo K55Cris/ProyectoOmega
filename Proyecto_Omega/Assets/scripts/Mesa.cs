8fca4f5b86ef3c0d9684836849e518d0458f3b8e
DarkCherry <thebigdarkcherry@gmail.com>
2017-12-31 10:21:03 -0300

    no message

--- Proyecto_Omega/Assets/scripts/Mesa.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Mesa : MonoBehaviour {
-    public abstract void Jugar(DragTest carta);
-    public abstract void Quitar(DragTest carta);
-}
