0f00f16bbef1818275930d9407b722078d6670b9
DarkCherry <thebigdarkcherry@gmail.com>
2017-12-31 10:21:57 -0300

    no message

---
+++ Proyecto_Omega/Assets/scripts/Deck.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class Carta : MonoBehaviour
+{
+    public string[] Code;
+}
+public class Deck : MonoBehaviour {
+    private List<string> deck = new List<string>();
+    //este seria el metodo de cargar, SI TUVIERA UNO!
+    private void ReadFromLine()
+    {
+        StreamReader reader = new StreamReader("/Users/DK_RZ/Documents/scripts/Assets/deck/Deck.txt");
+        string s;
+        while ((s = reader.ReadLine()) != null)
+        {
+            print(s);
+            deck.Add(s);
+        }
+    }
+    private void OnMouseUp()
+    {
+        //print(RobarEspecifico("st-1"));
+        //print(Robar());
+        string robada = Robar();
+        if (!robada.Equals("")) {
+            GameObject.Find("DarkArea").GetComponent<DarArea>().Meter(robada);
+        }
+        else
+        {
+
+            print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            Mezclar();
+        }
+    }
+    void Mezclar()
+    {
+        print("a");
+        for (int i = 29; i > 1; i--)
+        {
+            int aleatorio = Random.Range(0, 29);
+            string temp = deck[i];
+            deck[i] = deck[aleatorio];
+            deck[aleatorio] = temp;
+        }
+    }
+    string Robar()
+    {
+        try
+        {
+            string x = deck[0];
+            deck.RemoveAt(0);
+            return x;
+        }
+        catch (System.ArgumentOutOfRangeException)
+        {
+            deck = GameObject.Find("DarkArea").GetComponent<DarArea>().Devolver();
+            Mezclar();
+            return ("");
+        }
+
+    }
+    public string RobarEspecifico(string codCarta)
+    {
+        int i = 0;
+        foreach (var item in deck)
+        {
+            print(item + " " + codCarta);
+            if (item.Equals(codCarta))
+            {
+                string x = deck[i];
+                deck.RemoveAt(i);
+                return x;
+            }
+            i++;
+        }
+        throw new System.Exception();
+    }
+
+    // Use this for initialization
+    void Start() {
+        ReadFromLine();
+        //yo actualmente cargo el deck desde otra clase
+        //deck = CNTdrag.deck;
+        foreach (var item in deck)
+        {
+            print(item);
+        }
+        Mezclar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
---
+++ Proyecto_Omega/Assets/scripts/Movimiento.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    public float y = 0;
+    private Ray ray;
+    private Canvas a = new Canvas();
+    public GameObject particle;
+    private void Click()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            print(ray);
+            if (!Physics.Raycast(ray))
+            {
+                //Si no hay, creas un objeto particle, en la posicion del mouse
+                Instantiate(particle, Input.mousePosition, transform.rotation);
+            }
+        }
+    }
+    float distancia;
+
+    void OnMouseDown()
+    {
+
+        this.distancia = Vector3.Distance(Camera.main.transform.position, this.transform.position);
+    }
+
+    void OnMouseDrag()
+    {
+
+        Vector3 temp = Input.mousePosition;
+        temp.z = this.distancia;
+        print(temp.x);
+        print(temp.y);
+        print(temp.z);
+        print(temp);
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+
+    }
+    private void OnMouseUp()
+    {
+        print(transform.position);
+    }
+    /*private void OnMouseDown()
+    {
+        Click();
+        print(ray.origin);
+        transform.position = ray.origin;
+        transform.Rotate(90,0,0);
+        //transform.Translate(0, y, 0);
+    }*/
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
