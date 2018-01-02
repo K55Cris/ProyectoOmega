fc03ed0a70e67b5c7e448979584f693fd7e3480c
SilentDefault <silentdefault@gmail.com>
2018-01-02 02:43:51 -0600

    MainMenu v2.2
    -ahora el menu usa un LoaderScreen al cargar las demas scenas.
    -corregí el nombre del objeto "LevelLoader" le faltaba la "A"
    -vincule dicho objeto al script que le corresponde.

--- Proyecto_Omega/Assets/scripts/MainMenu.cs
+++ Proyecto_Omega/Assets/scripts/MainMenu.cs
@@ -5,20 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    public void Deck()
-    {
-        SceneManager.LoadScene("Deck");
-    }
-
-    public void Tamer()
-    {
-        //
-    }
-
-    public void IA()
-    {
-        SceneManager.LoadScene("vsIA");
-    }
+    //Comentario Random
 
     public void Quit()
     {
