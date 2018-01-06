5a096f39477f0ce184956245b764fe8c9e49b55a
Joselu <joseludam2000@gmail.com>
2018-01-06 06:53:24 +0100

    Modificado script para la carga de escenas
    Modificado el metodo para cargar escenas, ahora se requiere como
    parametro un string con el nombre de la escena que se desea cargar.

--- Proyecto_Omega/Assets/scripts/LevelLoader.cs
+++ Proyecto_Omega/Assets/scripts/LevelLoader.cs
@@ -4,24 +4,15 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour {
-    public GameObject loadingScreen;
-    public Slider slider;
-    public void loadLevel(int sceneIndex) 
+
+    /// <summary>
+    /// Carga la escena deseada
+    /// </summary>
+    /// <param name="nombreDeLaEscena">
+    /// String con el nombre de la escena en el proyecto
+    /// </param>
+    public void CargarEscena(string nombreDeLaEscena)
     {
-        StartCoroutine(LoadAsynchronouly(sceneIndex));
-    }
-
-    IEnumerator LoadAsynchronouly(int sceneIndex) 
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-
-        loadingScreen.SetActive(true);
-        while (!operation.isDone) 
-        {
-            float progress = Mathf.Clamp01(operation.progress / .9f);
-            slider.value = progress;
-
-            yield return null;
-        }
+        SceneManager.LoadScene(nombreDeLaEscena);
     }
 }
