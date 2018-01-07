42440fec111fd74de45aa7691bcd91c9b18017a4
DarkCherry <thebigdarkcherry@gmail.com>
2018-01-07 08:44:59 -0300

    Reparado el robo y comentado

--- Proyecto_Omega/Assets/scripts/Deck.cs
+++ Proyecto_Omega/Assets/scripts/Deck.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 public class Deck : MonoBehaviour {
+    [TooltipAttribute("Modelado de la carta.")]
     public GameObject modeladoDeLaCarta;
+    [TooltipAttribute("Ingresa la mainCamera.")]
     public Camera camara = new Camera();
+    [TooltipAttribute("Deck del jugador/Bot/cualquier deck")]
     public static Deck instance;
     private List<Sprite> arrayImage;
     private List<string> deck = new List<string>();
@@ -13,13 +16,19 @@ public class Deck : MonoBehaviour {
     {
         instance = this;
     }
+    //Al cliker sobre el deck realiza esto:
     private void OnMouseUp()
     {
+        //revista si el deck esta bacio
         if (!(deck == null))
         {
+            //mira la mano y revisa si esta llena
             if (!(PosicionDeLasCartas.GetCantidadActualDeCartas() == 6))
             {
+                //roba el codigo
                 string robada = Robar();
+                //carga los datos (en este caso solo las imagenes, deberia llamar a DataManager y pedir la carta y su info o cargar los datos al deck y solo robar hmm)
+                //Y compara todas las imagenes en busca de la necesaria (Si es un asco pero era para probar)
                 Texture imagenRobada = new Texture();
                 foreach (var item in arrayImage)
                 {
@@ -29,16 +38,18 @@ public class Deck : MonoBehaviour {
                         break;
                     }
                 }
+                //instancia la carta con su modelado y se le dan sus valores
                 GameObject carta = Instantiate(modeladoDeLaCarta);
                 carta.GetComponent<DragTest>().SetCamera(camara);
                 carta.GetComponent<Renderer>().material.mainTexture = imagenRobada;
                 carta.name = ("Carta" + (PosicionDeLasCartas.GetCantidadActualDeCartas() + 1));
+                //posiciona la carta
                 PosicionDeLasCartas.AumentarCarta();
             }
         }
         else
         {
-            print("El deck esta vacio, se volvera a cargar");
+            //si esta vacio en vez de robar carga el deck, y lo mezcla
             deck = GameObject.Find("DarkArea").GetComponent<DarArea>().Devolver();
             FisherYates(deck);
         }
@@ -49,24 +60,29 @@ public class Deck : MonoBehaviour {
         deck[primero] = deck[segundo];
         deck[segundo] = temp;
     }
+    //mezcla el deck
     public void FisherYates(List<string> deck)
     {
         for (int i = deck.Count - 1; i > 0; i--)
         {
             int index = Random.Range(0, i);
+            //lo asigna
             Swap(deck, i, index);
         }
     }
+    //devuelve el codigo de una carta
     string Robar()
     {
         try
         {
+            //devuelve la primera carta
             string x = deck[0];
             deck.RemoveAt(0);
             return x;
         }
         catch (System.ArgumentOutOfRangeException)
         {
+            //en caso de error por estar vacio devuelve se le asigna null para compararlo mas tarde
             deck = null;
             return null;
         }
@@ -78,6 +94,7 @@ public class Deck : MonoBehaviour {
         }*/
 
     }
+    // si hay cartas que roben una carta en espesifico y para sacar del deck el rookie inicial
     public string RobarEspecifico(string codCarta)
     {
         int i = 0;
@@ -93,7 +110,7 @@ public class Deck : MonoBehaviour {
         }
         throw new System.Exception();
     }
-
+    // Getters and Setters
     public List<string> GetDeck()
     {
         return deck;
